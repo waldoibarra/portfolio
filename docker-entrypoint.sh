d5d55b546aea1faab328b236256b4ac0bec0859c
Waldo Ibarra <waldo.ibarra@linux.com>
2022-12-15 15:24:41 -0600

    perf: Reduce run time on container start
    This to make one off commands ran on container to be faster, such as
    linting and testing during the pre-commit hook, to make it faster when
    doing these commits.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,44 @@
 #!/bin/sh
 set -e
 
-command=$@
+node_modules_directory=/app/node_modules
 
-# Copy node_modules directory from cache for it to be available on host machine.
-echo "Syncing node_modules directory."
-rsync -ar /cache/node_modules/. /app/node_modules
+sync_node_modules() {
+    echo "Syncing node_modules directory from cache for it to be available on host machine."
+    rsync -ar /cache/node_modules/. $node_modules_directory
+}
 
-# Enable Git hooks.
-npx husky install
+sync_node_modules_if_not_present() {
+    if [ ! -d $node_modules_directory ];
+    then
+        sync_node_modules
+    fi
+}
 
-if [ -z "$command" ];
-then
+enable_git_hooks() {
+    npx husky install
+}
+
+start_website() {
+    sync_node_modules
+    enable_git_hooks
     exec npm start
-else
-    exec $command
-fi
+}
+
+run_one_off_command() {
+    sync_node_modules_if_not_present
+    exec $@
+}
+
+main() {
+    local command=$@
+
+    if [ -z "$command" ];
+    then
+        start_website
+    else
+        run_one_off_command $command
+    fi
+}
+
+main $@
