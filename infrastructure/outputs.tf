b61212e0d33047a4a6f596ed7886948e0a006b75
Waldo Ibarra <waldo.ibarra@linux.com>
2026-05-03 21:10:26 -0600

    chore: Destroy module and remove main.tf
    - terraform destroy: 12 resources destroyed
    - Delete infrastructure/main.tf (module call)
    - Delete .terraform.lock.hcl (old module lock)
    - Strip profile lines from provider.tf
    - Empty vars.tf and outputs.tf (module refs gone)

--- infrastructure/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "cloudfront_distribution_id" {
-  description = "The CloudFront Distribution ID for cache invalidation"
-  value       = module.s3-cloudfront-static-website_example.cloudfront_distribution_id
-}
--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,10 +1,3 @@
 provider "aws" {
-  region  = "us-west-2"
-  profile = ""
-}
-
-provider "aws" {
-  alias   = "useast1"
-  region  = "us-east-1"
-  profile = ""
+  region = "us-west-2"
 }
