f926aabb57f45180a5b88ebb1fc871f4ff7f105b
Waldo Ibarra <waldo.ibarra@linux.com>
2026-05-03 22:27:29 -0600

    fix: Allow overwrite on ACM validation CNAME

--- infrastructure/acm.tf
+++ infrastructure/acm.tf
@@ -21,11 +21,12 @@ resource "aws_route53_record" "cert_validation" {
     }
   }
 
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  ttl     = 60
-  records = [each.value.record]
+  zone_id         = data.aws_route53_zone.main.zone_id
+  name            = each.value.name
+  type            = each.value.type
+  ttl             = 60
+  records         = [each.value.record]
+  allow_overwrite = true
 }
 
 resource "aws_acm_certificate_validation" "site" {
